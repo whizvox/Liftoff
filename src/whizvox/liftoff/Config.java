b02d6f9c7d93e79cb3cb3f1f1279744a3cb9b3c7
Cornelius Eanes <corneliuseanes@gmail.com>
2015-12-08 09:41:26 -0800

    Invalid config values now are updated

--- src/whizvox/liftoff/Config.java
+++ src/whizvox/liftoff/Config.java
@@ -14,12 +14,16 @@ public class Config {
         signRange = plugin.getConfig().getInt("signRange");
         if (signRange > 255 || signRange < 3) {
             System.out.println("[Liftoff] Sign range can only be between 3 and 255: " + signRange);
-            plugin.getConfig().set("signRange", defaults.get("signRange"));
+            int signRange_default = defaults.getInt("signRange");
+            plugin.getConfig().set("signRange", signRange_default);
+            signRange = signRange_default;
         }
         solidGroundRange = plugin.getConfig().getInt("solidGroundRange");
         if (solidGroundRange > 255 || signRange < 1) {
             System.out.println("[Liftoff] Solid ground range can only be between 1 and 255: " + solidGroundRange);
-            plugin.getConfig().set("solidGroundRange", defaults.get("solidGroundRange"));
+            int solidGroundRange_default = defaults.getInt("solidGroundRange");
+            plugin.getConfig().set("solidGroundRange", solidGroundRange_default);
+            solidGroundRange = solidGroundRange_default;
         }
         signColoring = plugin.getConfig().getBoolean("signColoring");
         plugin.saveConfig();
